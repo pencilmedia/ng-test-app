79a6a0ca9a5bb1e21800aa9cc3304f4f53cb248d
ralph <ralph.garcia@ivanti.com>
2017-11-15 13:57:43 -0800

    updates

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // server.js
 const express = require('express');
+
+
+const path = require('path');
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
