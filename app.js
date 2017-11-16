809bc71508e0b07329439bbdec34f4146b5cae44
ralph <ralph.garcia@ivanti.com>
2017-11-15 16:15:18 -0800

    change port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || '3001';
+const port = process.env.PORT || '8080';
 app.set('port', port);
 
 const server = http.createServer(app);
