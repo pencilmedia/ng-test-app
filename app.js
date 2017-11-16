7fe82f1ceac8a2be5c08a69e2fcc983671621dbd
ralph <ralph.garcia@ivanti.com>
2017-11-15 16:20:17 -0800

    adding procfile and adjust package.json

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3001';
 app.set('port', port);
 
 const server = http.createServer(app);
